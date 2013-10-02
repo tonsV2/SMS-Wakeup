e3f0bf63d7babd6dcf38a2fae7a6af0561a173a4
Super Snot <sebastianthegreatful@gmail.com>
2013-10-02 22:51:48 +0200

    Imported misc. code from old repository.

--- SMSWakeUp/src/main/java/com/snot/smswakeup/MainActivity.java
+++ SMSWakeUp/src/main/java/com/snot/smswakeup/MainActivity.java
@@ -4,20 +4,43 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import android.view.MenuItem;
+import android.content.Intent;
+
+
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch(item.getItemId())
+		{
+		case R.id.action_settings:
+			Intent settings = new Intent(this, Preferences.class);
+			startActivity(settings);
+			return true;
+//		case R.id.action_add_contact:
+//			Intent contact = new Intent(Intent.ACTION_PICK, Contacts.CONTENT_URI);
+//			startActivityForResult(contact, R.id.action_add_contact);
+//			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
 }
+
---
+++ SMSWakeUp/src/main/java/com/snot/smswakeup/Preferences.java
@@ -0,0 +1,27 @@
+package com.snot.smswakeup;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity {
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Display the fragment as the main content.
+//maingetFragmentManager().beginTransaction().replace(android.R.id.content, new PreferencesFragment()).commit();
+//PreferencesFragmentPreferenceManager.setDefaultValues(Preferences.this, R.menu.preferences, false);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+/*
+	public static class PreferencesFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.menu.preferences);
+        }
+    }
+*/
+}
+
